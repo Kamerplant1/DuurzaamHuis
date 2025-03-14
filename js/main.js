9cd73fe82b51f455af18459e3e67a18edc065016
Jasper <39047@ma-web.nl>
2025-03-14 12:53:34 +0100

    dag sensor main page layout finished

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function updateTime() {
+    const now = new Date();
+    
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+
+    document.getElementById('time').textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+function updateImage() {
+    const img = document.getElementById("dynamicImage");
+    if (!img) return;
+    
+    const now = new Date();
+    const hours = now.getHours();
+    
+    img.src = (hours >= 19 || hours < 7) ? "img/moon.png" : "img/sun.webp";
+}
+
+updateTime();
+updateImage();
+setInterval(updateTime, 1000);
+setInterval(updateImage, 60000);
