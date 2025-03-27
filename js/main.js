5a1658e0cc5a1b670577b5d35eb1d409b9d82928
Tristan Wals <39040@ma-web.nl>
2025-03-27 23:05:54 +0100

    ActualTemperatureWidget

--- js/main.js
+++ js/main.js
@@ -26,3 +26,32 @@ updateImage();
 setInterval(updateTime, 1000);
 setInterval(updateImage, 60000);
 
+// Dit stuk hierboven is voor de tijdbalk die heeft jasper gemaakt.
+const buitentemperatuur = document.getElementById("js--actuelebuitentemp");
+const weatherImage = document.getElementById("js--weatherImg");
+const weatherType = document.getElementById("js--weatherType");
+
+function updateWeatherData() {
+    fetch("https://weerlive.nl/api/weerlive_api_v2.php?key=demo&locatie=Amsterdam")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (realTempData) {
+            const tempData = realTempData.liveweer[0];
+            const temperature = tempData.temp;
+
+            buitentemperatuur.innerText = temperature + "°C";
+            weatherType.innerText = tempData.samenv;
+
+            if (temperature < 10) {
+                weatherImage.src = "../img/cold.png";
+            } else {
+                weatherImage.src = "../img/warm.png";
+            }
+        });
+}
+setInterval(updateWeatherData, 3000);
+updateWeatherData();
+
+// dit hierboven is voor de weather api voor actuele buitentemperatuur (Van Tristan).
+
